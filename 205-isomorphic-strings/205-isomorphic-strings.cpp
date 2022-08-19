19f46bb36a81ce2b200822812db2a5dc2ef1724a
Ahmed Ashraf Marzouk <82055675+Ahmed-Ashraf-Marzouk@users.noreply.github.com>
2022-08-19 10:57:33 -0700

    Time: 8 ms (70.94%), Space: 6.8 MB (98.42%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        
-        
         if (s.size() != t.size()) return false; 
         int l = 0,r = 0;
         for (int i = 1; i < s.size(); i++)
@@ -10,6 +8,7 @@ public:
             if (l != r) return false; 
             if (s[i] != s[i-1]) l = 1; else l = 0;
             if (t[i] != t[i-1]) r = 1; else r = 0;
+            // to check each pair is the same 
             if (t[s.find(s[i])] != t[i]) return false;
             if (s[t.find(t[i])] != s[i]) return false;
         }
