707a70924085f3b60d5c8a4fb55af1412c9ebeab
Ahmed Ashraf Marzouk <82055675+Ahmed-Ashraf-Marzouk@users.noreply.github.com>
2022-08-18 17:09:54 -0700

    Time: 5 ms (48.62%), Space: 8.5 MB (75.23%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        
+        for (auto i = (nums.begin() + 1); i < nums.end(); i++)
+        {
+            *i = *i + *(i - 1);
+            // cout << *i << endl;
+        }
+        
+        return nums;
+    }
+};
