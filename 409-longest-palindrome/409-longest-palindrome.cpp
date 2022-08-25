78743c40f46d46fb3e15e13cd9c040ea4827f9ea
Ahmed Ashraf Marzouk <82055675+Ahmed-Ashraf-Marzouk@users.noreply.github.com>
2022-08-25 15:05:37 -0700

    Time: 7 ms (37.23%), Space: 6.6 MB (81.39%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int f_l[26] = {0};
+        int f_u[26] = {0};
+        
+        for (int i = 0; i < s.size(); i++)
+        {
+            if(isupper(s[i]))
+            {
+                f_u[s[i] - 'A']++;
+            }
+            else f_l[s[i] - 'a']++;
+        }
+        int b_odd = 0;
+        int sum = 0;
+        int f = 0;
+        // for (int i = 0; i < 26; i++)
+        // {
+        //     if(f_u[i] % 2 != 0)
+        //     {
+        //         if(f_u[i] > b_odd)
+        //             b_odd = f_u[i];
+        //     }
+        //     if(f_l[i] % 2 != 0)
+        //     {
+        //         if(f_l[i] > b_odd)
+        //             b_odd = f_l[i];
+        //     }
+        // }
+        for (int i = 0; i < 26; i++)
+        {
+            if (f_l[i] != 1)
+            {
+                sum += f_l[i]%2 != 0 ? f_l[i] - 1 : f_l[i];
+            }
+            if (f_u[i] != 1)
+                sum += f_u[i]%2 != 0 ? f_u[i] - 1 : f_u[i];
+            if (f_u[i] %2 != 0 || f_l[i] %2 != 0) b_odd = 1;
+
+        }
+        
+  
+        return sum + b_odd;
+        
+    }
+  
+};
+
+// if even number of letter with 1/0 odd number of a  one letter ! 
